89b167afbdf1847b55864de824ee2264ba16e988
Estrada <javier.estrada@cgi.com>
2022-01-28 15:17:24 -0600

    Feat: Adding my gitignore file

--- src/com/company/BinarySearchExample.java
+++ src/com/company/BinarySearchExample.java
@@ -1,2 +1,12 @@
-package com.company;public class BinarySearchExample {
+package com.company;
+
+import java.util.Arrays;
+
+public class BinarySearchExample {
+    public static void main(String[] args) {
+
+        int[] numbers = {2,4,6,8};
+        Arrays.binarySearch(numbers, 2);
+        System.out.println(Arrays.binarySearch(numbers, 2));
+    }
 }
